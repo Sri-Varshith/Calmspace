1e342c932ebac6299d0b96b2a5369fa05bb93b60
Sri-Varshith <nvarshith.2007@gmail.com>
2026-03-09 15:09:43 +0530

    added images and audio

--- calmspace/lib/main.dart
+++ calmspace/lib/main.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'shared/theme/app_theme.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // Ensures Flutter engine is ready before we do anything
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    // ProviderScope is the heart of Riverpod
+    // It must wrap your entire app
+    // Without this, NO provider will work
+    const ProviderScope(
+      child: CalmSpaceApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CalmSpaceApp extends StatelessWidget {
+  const CalmSpaceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CalmSpace',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      theme: AppTheme.darkTheme,
+      home: const Scaffold(
+        backgroundColor: Color(0xFF0D0D1A),
         body: Center(
-          child: Text('CalmSpace'),
+          child: Text(
+            'CalmSpace',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
         ),
       ),
     );
